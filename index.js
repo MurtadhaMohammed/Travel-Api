dff8c494844a2fbcddc28c980562f637919b94cb
MurtadhaMohammed <mumu9943@gmail.com>
2023-07-03 18:14:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+//Trips Routes =======================================
+app.get("/api/trips", async (req, res) => {
+  let resp = {};
+  try {
+    const trips = await prisma.trip.findMany({});
+    resp = { records: trips, success: true };
+  } catch (error) {
+    resp = { err: error.message, success: false };
+  }
+  res.json(resp);
+});
+
+app.post("/api/trips", async (req, res) => {
+  let resp = {};
+  try {
+    const trips = await prisma.trip.create({ data: req.body });
+    resp = { records: trips, success: true };
+  } catch (error) {
+    resp = { err: error.message, success: false };
+  }
+  res.json(resp);
+});
+
+app.put("/api/trips/:id", async (req, res) => {
+  let resp = {};
+  try {
+    const trips = await prisma.trip.update({
+      where: { id: Number(req.params["id"]) },
+      data: req.body,
+    });
+    resp = { records: trips, success: true };
+  } catch (error) {
+    resp = { err: error.message, success: false };
+  }
+  res.json(resp);
+});
+
+app.delete("/api/trips/:id", async (req, res) => {
+  let resp = {};
+  try {
+    const trips = await prisma.trip.delete({
+      where: {
+        id: Number(req.params["id"]),
+      },
+    });
+    resp = { records: trips, success: true };
+  } catch (error) {
+    resp = { err: error.message, success: false };
+  }
+  res.json(resp);
+});
+
+//Bookings Routes ====================================
+app.get("/api/bookings", async (req, res) => {
+  let resp = {};
+  try {
+    const bookings = await prisma.booking.findMany({ include: { trip: true } });
+    resp = { records: bookings, success: true };
+  } catch (error) {
+    resp = { err: error.message, success: false };
+  }
+  res.json(resp);
+});
+
+app.post("/api/bookings", async (req, res) => {
+  let resp = {};
+  try {
+    const trips = await prisma.booking.create({ data: req.body });
+    resp = { records: trips, success: true };
+  } catch (error) {
+    resp = { err: error.message, success: false };
+  }
+  res.json(resp);
+});
+
+app.put("/api/bookings/status/:id", async (req, res) => {
+  // Status should be: BENING || ACCEPTED || REJECTED
+  let resp = {};
+  try {
+    const trips = await prisma.booking.update({
+      where: {
+        id: Number(req.params["id"]),
+      },
+      data: {
+        status: req?.body?.status,
+      },
+    });
+    resp = { records: trips, success: true };
+  } catch (error) {
+    resp = { err: error.message, success: false };
+  }
+  res.json(resp);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ prisma/migrations/20230703141431_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "Trip" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "price" INTEGER NOT NULL,
+    "img" TEXT,
+    "place" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "days" INTEGER NOT NULL,
+
+    CONSTRAINT "Trip_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Booking" (
+    "id" SERIAL NOT NULL,
+    "customerName" TEXT NOT NULL,
+    "customerPhone" TEXT NOT NULL,
+    "customerAddress" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "tripId" INTEGER NOT NULL,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_tripId_fkey" FOREIGN KEY ("tripId") REFERENCES "Trip"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
